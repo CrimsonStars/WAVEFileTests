0b9ce1ecb3caa34d51bc0f578110c0134abfc1ef
CrimsonStars <34871858+CrimsonStars@users.noreply.github.com>
2022-07-06 17:40:37 +0200

    Add project files.

---
+++ WAVEFileTests/Model/WaveData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WAVEFileTests.Model
+{
+    public class WaveData
+    {
+        public string Name { get; set; }
+        public int Bitrate { get; set; }
+        public int Length { get; set; }
+        public int Frequency { get; set; }
+
+        public WaveData()
+        {
+            Name = "Sample_wave_file";
+            Bitrate = 0;
+            Length = 0;
+            Frequency = 440;
+        }
+    }
+}
---
+++ WAVEFileTests/Model/WaveFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WAVEFileTests.Model
+{
+    internal class WaveFile
+    {
+        public WaveData Data { get; private set; }
+        public string Folder { get; set; }
+        public string Filename { get; set; }
+
+        public WaveFile(string FOLDER, string FILENAME, WaveData DATA)
+        {
+            this.Folder = FOLDER;
+            this.Filename = FILENAME;
+            this.Data = DATA;
+        }
+
+        public void GenerateFile()
+        {
+            FileStream fs = File.Create(Path.Combine(Folder, Filename, ".wav"));
+
+
+
+            fs.Close();
+        }
+    }
+}
